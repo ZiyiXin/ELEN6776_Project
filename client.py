1f4e413dc2e1e74aa1b9019f26e6dd8a46a26960
OTOFF <mainlieeee@gmail.com>
2024-12-04 05:37:45 +0800

    modified the client service

--- client.py
+++ client.py
@@ -10,77 +10,41 @@ def request_file(filename, serverPort):
 
     client_socket.sendto(filename)
 
-    response = client_socket.recv(2048)
-    
-    if response
+    file_content = ''
+    while True:
+        response = client_socket.recv(2048)
+        if not response:
+            break
+        file_content += response.decode()
+    print(file_content)
 
 
+def print_welcome_message():
+    welcome_message = """
+###############################################################
+#                                                         #
+#     Welcome to Cache Simulator by Ziyi Xin and Meng Li! #
+#   Here you can type in the lyrics of OSTs of many TV    #
+#   shows you want. Come and try it out!                  #
+#                                                         #
+###############################################################
+"""
+    print(welcome_message)
 
 
 if __name__ == "__main__":
+    print_welcome_message()
     server_ip = sys.argv[1]
     listen_port = sys.argv[2]
-
-    filename = input("Enter the filename to request: ")
-    request_file(filename, server_ip, listen_port)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Directory to save downloaded files
-# DOWNLOADS_DIR = "./Clients"
-# os.makedirs(DOWNLOADS_DIR, exist_ok=True)
-
-# def request_file(filename):
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect((SERVER_HOST, SERVER_PORT))
-
-#     try:
-#         # Send the filename request to the server
-#         print(f"Client sending filename: {filename}")
-#         client_socket.send(filename.encode())
-
-#         # Receive response from the server (FOUND or NOTFOUND)
-#         response = client_socket.recv(BUFFER_SIZE).decode()
-#         print(f"Client received response: {response}")
-        
-#         if response == "FOUND":
-#             # Receive file size from the server
-#             file_size = int(client_socket.recv(BUFFER_SIZE).decode())
-#             print(f"Client received file size: {file_size} bytes. Downloading...")
-
-#             # Set the path to save the downloaded file in the downloads directory
-#             download_path = os.path.join(DOWNLOADS_DIR, filename)
-
-#             # Open the file to write the received content
-#             with open(download_path, "wb") as f:
-#                 bytes_received = 0
-#                 while bytes_received < file_size:
-#                     bytes_read = client_socket.recv(BUFFER_SIZE)
-#                     if not bytes_read:
-#                         break
-#                     f.write(bytes_read)
-#                     bytes_received += len(bytes_read)
-#                     print(f"Received {bytes_received}/{file_size} bytes...")  # Progress output
-
-#             if bytes_received == file_size:
-#                 print(f"File '{filename}' downloaded successfully to '{DOWNLOADS_DIR}'.")
-#             else:
-#                 print(f"Download incomplete. Expected {file_size} bytes, received {bytes_received} bytes.")
-#         else:
-#             print(f"File '{filename}' not found on the server.")
-    
-#     except Exception as e:
-#         print(f"Error: {e}")
-#     finally:
-#         client_socket.close()
+    while True:
+        try:
+            filename = input("What do you what to check: ")
+            request_file(filename, server_ip, listen_port)
+            deter = input("Are you still looking for something else?(y/n): ")
+            if deter.lower() == "n":
+                break
+        except KeyboardInterrupt:
+            print("\nExiting gracefully.")
+            break
+        except Exception as e:
+            print(f"Wrong attempt, please try again later. Error: {e}")
